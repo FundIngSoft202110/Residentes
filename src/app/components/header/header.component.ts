292a35bfb3f4a4466422835ba99eac0d1721f28a
Sebastian Barreto <jsebastian.barretoj99@gmail.com>
2021-03-30 23:44:03 -0500

    Se corrigio boton menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-header',
@@ -11,10 +12,13 @@ export class HeaderComponent implements OnInit {
 
   @Input() menu : string;
 
-  constructor() { 
+  constructor( private menuCtrl: MenuController) { 
   }
 
   ngOnInit() {}
 
+  toggleMenu(){
+    this.menuCtrl.toggle();
+  }
 
 }
