8d0da45f3f75cdb28a311d2e2d07ca1a1ae6819a
Santiago Quintana E <quintana.santiago@outlook.com>
2021-04-19 10:33:05 -0500

    correct servi agregar conjunto

--- src/app/components/admin/agregar-conjunto-component/agregar-conjunto-component.component.ts
+++ src/app/components/admin/agregar-conjunto-component/agregar-conjunto-component.component.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { DxCheckBoxModule, DxFormComponent } from 'devextreme-angular';
 import { AppComponent } from 'src/app/app.component';
 import notify from 'devextreme/ui/notify';
-import { Customer, AgregarConjuntoService } from '../../../Services/agregar-conjunto.service';
+import { Customer, AgregarConjuntoService } from '../../../Services/agregar-conjunto/agregar-conjunto.service';
 
 
 const sendRequest = function(value) {
