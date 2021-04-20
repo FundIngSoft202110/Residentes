71ffe38efb62f774a3725159bf60f5f8d9f42f87
XingYi98 <69372306+XingYi98@users.noreply.github.com>
2021-04-20 03:52:55 -0500

    Se ajusto propuesta y se comenzo resultados

---
+++ src/app/components/residente/resultados-residente/resultados-residente.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resultados-residente',
+  templateUrl: './resultados-residente.component.html',
+  styleUrls: ['./resultados-residente.component.scss'],
+})
+export class ResultadosResidenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
