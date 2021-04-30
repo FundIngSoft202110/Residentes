04f04defa4e69997a38f617e7f9357972f933a1b
Alejandro Diaz <alejandrodiazlote@gmail.com>
2021-04-30 17:01:18 -0500

    nuevo servicio para las areas comunes de admin

--- FrontendResidentes/src/app/Services/areaComunAdminServices/agregar-area-comun-admin.service.ts
+++ FrontendResidentes/src/app/Services/areaComunAdminServices/agregar-area-comun-admin.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class AgregarAreaComunService {
+export class AgregarAreaComunAdminService {
 
   constructor() { }
 }
