9effe6e0d9015a0cf0627824fb1728bb3ad7c3c1
Sebastian Barreto <jsebastian.barretoj99@gmail.com>
2021-05-18 03:33:29 -0500

    Correciones IDK Ingreso

--- FrontendResidentes/src/app/Services/ingreAptoServ/serv-ing-apto.service.ts
+++ FrontendResidentes/src/app/Services/ingreAptoServ/serv-ing-apto.service.ts
@@ -7,36 +7,36 @@ import { Injectable } from '@angular/core';
 
 
 export class ServIngAptoService {
-  private  apartamentos:any;
+  private apartamentos: any;
   private idConjunto;
-  private  apartamentos1= [
+  private apartamentos1 = [
     {
       IdApartamento: 1,
       Torre: 1,
       Numero: 1,
       Contrasena: 'abcd132',
-      Piso: 1 
+      Piso: 1
     },
     {
       IdApartamento: 2,
       Torre: 1,
       Numero: 2,
       Contrasena: 'sdfa234',
-      Piso: 1 
+      Piso: 1
     },
     {
       IdApartamento: 3,
       Torre: 1,
       Numero: 1,
       Contrasena: '12345678',
-      Piso: 2 
+      Piso: 2
     },
     {
       IdApartamento: 4,
       Torre: 1,
       Numero: 2,
       Contrasena: 'i7tyur456',
-      Piso: 2 
+      Piso: 2
     },
 
     {
@@ -44,94 +44,94 @@ export class ServIngAptoService {
       Torre: 2,
       Numero: 1,
       Contrasena: 'contraaa',
-      Piso: 1 
+      Piso: 1
     },
     {
       IdApartamento: 6,
       Torre: 2,
       Numero: 2,
       Contrasena: 'senaaa24',
-      Piso: 1 
+      Piso: 1
     },
     {
       IdApartamento: 7,
       Torre: 2,
       Numero: 1,
       Contrasena: 'poiuytred',
-      Piso: 2 
+      Piso: 2
     },
     {
       IdApartamento: 8,
       Torre: 2,
       Numero: 2,
       Contrasena: 'tgrgwr5325',
-      Piso: 2 
+      Piso: 2
     }
   ];
 
   constructor(private http: HttpClient) {
-    
+
   }
 
-  public getAptos(num:number) {
+  public getAptos(num: number) {
     this.getAptosU("localhost:8080/BackendResidentes/consultas/apartamentos/")
-        .subscribe(respuesta => {
-            console.log("subscirbe " + respuesta);
-            this.apartamentos=respuesta;
-    })
+      .subscribe(respuesta => {
+        console.log("subscirbe " + respuesta);
+        this.apartamentos = respuesta;
+      })
   }
 
-  public getAptosU(url:string){
+  public getAptosU(url: string) {
     return this.http.get(url);
- }
+  }
 
- setIdApto(idApto:number){
+  setIdApto(idApto: number) {
     window.localStorage['idApto'] = idApto.toString();
   }
-  idActualApto : any;
-  getIdApto(){
+  idActualApto: any;
+  getIdApto() {
     this.idActualApto = Number(window.localStorage['idApto'] || -1);
-    if(this.idActualApto == -1)
+    if (this.idActualApto == -1)
       return null;
     else
       return this.idActualApto;
   }
 
-  loginUser(credential, num:number){
+  loginUser(credential, num: number) {
     this.getAptos(num);
     console.log("other " + this.apartamentos);
     return new Promise((accept, reject) => {
-      let van =0;
-      let cont=0;
+      let van = 0;
+      let cont = 0;
       console.log("entre");
-      for(let ind of  this.apartamentos){
-        if( ( credential.torre == this.apartamentos[cont].torre) && ( credential.piso == this.apartamentos[cont].piso) && ( credential.numero == this.apartamentos[cont].numero) && ( credential.password == this.apartamentos[cont].contrasena) ){
-         
+      for (let ind of this.apartamentos) {
+        if ((credential.torre == this.apartamentos[cont].torre) && (credential.piso == this.apartamentos[cont].piso) && (credential.numero == this.apartamentos[cont].numero) && (credential.password == this.apartamentos[cont].contrasena)) {
+
           accept("Login correcto");
-          van =1;
+          van = 1;
         }
         cont++;
       }
-      if(van == 0){
+      if (van == 0) {
         reject("Login incorrecto");
       }
     });
   }
 
-  loginUserP(credential){
+  loginUserP(credential) {
     return new Promise((accept, reject) => {
-      let van =0;
-      let cont=0;
-      for(let ind of  this.apartamentos1){
+      let van = 0;
+      let cont = 0;
+      for (let ind of this.apartamentos1) {
 
-        if( ( credential.torre == this.apartamentos1[cont].Torre) && ( credential.piso == this.apartamentos1[cont].Piso) && ( credential.numero == this.apartamentos1[cont].Numero) && ( credential.password == this.apartamentos1[cont].Contrasena) ){
+        if ((credential.torre == this.apartamentos1[cont].Torre) && (credential.piso == this.apartamentos1[cont].Piso) && (credential.numero == this.apartamentos1[cont].Numero) && (credential.password == this.apartamentos1[cont].Contrasena)) {
           this.setIdApto(this.apartamentos1[cont].IdApartamento);
           accept("Login correcto");
-          van =1;
+          van = 1;
         }
         cont++;
       }
-      if(van == 0){
+      if (van == 0) {
         reject("Login incorrecto");
       }
     });
