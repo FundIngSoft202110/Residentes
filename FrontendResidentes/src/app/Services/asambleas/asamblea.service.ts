45e280169f22e52a97b9bd80621e54175d714e26
Alejandro Diaz <alejandrodiazlote@gmail.com>
2021-05-31 11:06:13 -0500

    asamblea admin

--- FrontendResidentes/src/app/Services/asambleas/asamblea.service.ts
+++ FrontendResidentes/src/app/Services/asambleas/asamblea.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { NULL_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Injectable } from '@angular/core';
-import { IPRESIDENTES } from 'src/app/constants';
+import { IPRESIDENTES, IPRESIDENTESA } from 'src/app/constants';
 
 export class AsambleaPK{
     idAsamblea: number;
@@ -29,6 +29,7 @@ let Nopciones: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 	providedIn: 'root'
 })
 export class AsambleaService {
+    private asambleaEstado:string;
 
 	private asambleas: any;
 
@@ -49,12 +50,21 @@ export class AsambleaService {
 			return this.asambleaAbierta;
 	}// getAsambleaAbierta
 
+	getAsambleaEstado() {
+		this.asambleaEstado = window.localStorage['asambleaEstado'] ;
+			return this.asambleaEstado;
+	}// getAsambleaAbierta
+
+	setAsambleaEstado(estado: string) {
+		window.localStorage['asambleaEstado'] = estado;
+	}// setAsambleaAbierta
+
 	public getAsambleaUrl(url: string) {
 		return this.http.get(url);
 	} // end getConjuntoUrl
 
 	async cargarAsambleas(numConjunto: number) {
-		this.getAsambleaUrl(IPRESIDENTES + "consultas/asambleas/" + numConjunto.toString())
+		this.getAsambleaUrl(IPRESIDENTESA + "/consultas/asambleas/" + numConjunto.toString())
 			.subscribe(respuesta => {
 				this.asambleas = respuesta;
 			})
---
+++ FrontendResidentes/src/app/components/admin/lista-asambleas-admin/lista-asasamblea-admin/lista-asasamblea-admin.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, AlertController } from '@ionic/angular';
+import { AsambleaService, Asamblea } from 'src/app/Services/asambleas/asamblea.service';
+import { ConjuntosService } from 'src/app/Services/conjuntos/conjuntos.service';
+
+@Component({
+  selector: 'app-lista-asasamblea-admin',
+  templateUrl: './lista-asasamblea-admin.component.html',
+  styleUrls: ['./lista-asasamblea-admin.component.scss'],
+})
+export class ListaAsasambleaAdminComponent implements OnInit {
+
+  asambleas = [];
+  private idConjunto:number;
+
+  constructor(private asambleaService: AsambleaService, private navCtrl: NavController, 
+              private conjuntosService:ConjuntosService, public alertController: AlertController) { }
+
+  ngOnInit() {
+  }// end ngOnInit
+
+  async waitBD(){
+    await new Promise(resolve => setTimeout(resolve, 600));
+  }
+
+  async ionViewWillEnter(){
+    this.idConjunto = this.conjuntosService.getConjuntoActivo();
+    this.asambleaService.cargarAsambleas(this.idConjunto);
+    await this.waitBD();
+    this.asambleas = this.asambleaService.getAsambleas();
+    console.log("Asambleas: ", this.asambleas);
+  } // end ionViewWillEnter
+
+  goRoute(asamblea:Asamblea){
+     
+      console.log("Asamblea = ", asamblea.asambleaPK.idAsamblea, " Estado = ", asamblea.estado);
+      this.asambleaService.setAsambleaAbierta(asamblea.asambleaPK.idAsamblea);
+      this.asambleaService.setAsambleaEstado(asamblea.estado);
+      this.navCtrl.navigateForward("/asamblea-admin");
+    
+    
+  }
+
+  async presentAlertConfirmAsamblea() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Información',
+      message: 'No es posible acceder a asambleas programadas',
+      buttons: [
+        {
+          text: 'Aceptar',
+          cssClass: 'aceptar',
+          handler: () => {
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  } // end presentAlertConfirm
+
+  getAsambleaFecha(asamblea:Asamblea){
+    return this.conjuntosService.convertDate(asamblea.fecha);
+  } // end getAsambleaFecha
+
+  getAsambleaHora(asamblea:Asamblea){
+    return this.conjuntosService.convertHour(asamblea.hora);
+  } // end getAsambleaFecha
+
+  getAsambleaEstado(asamblea:Asamblea){
+    if(asamblea.estado == 'F')
+      return "Finalizada"
+    else if(asamblea.estado == 'P')
+      return "Programada"
+    else if(asamblea.estado == 'A')
+      return "Activa"
+  } // end getAsambleaEstado
+
+  botonNuevoPaquete(){
+      
+  }
+}
